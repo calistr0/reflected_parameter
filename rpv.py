f67f9cfc1a8e30936336dc0872e5af3ee0846fc6
calistr0 <88991900+calistr0@users.noreply.github.com>
2023-02-20 15:04:26 -0300

    Update rpv.py

--- rpv.py
+++ rpv.py
@@ -2,19 +2,22 @@ import requests, sys
 from termcolor import colored
 #programa para verificar se o parametro reflete na página
 
-arquivo = input(colored('Arquivo >>> ', 'green'))
-
-with open(arquivo, 'r') as arqv:
-    for lista in arqv.read().split('\n'):
-        try:
-            if '<content>' in requests.get(lista).text:
-                print(colored(f'{lista}', 'green'))
-                with open('rpv_response.txt', 'a') as rpv:
-                    rpv.write(f'\n{lista}')
-            else:
-                print(colored(f'{lista}', 'red'))
-        except KeyboardInterrupt:
-            print(colored('--- CLOSED ---', 'red'))
-            sys.exit(0)
+try:
+    arquivo = input(colored('Arquivo >>> ', 'green'))
+    with open(arquivo, 'r') as arqv:
+        for lista in arqv.read().split('\n'):
+            try:
+                if '<content>' in requests.get(lista).text:
+                    print(colored(f'{lista}', 'green'))
+                    with open('rpv_response.txt', 'a') as rpv:
+                        rpv.write(f'\n{lista}')
+                else:
+                    print(colored(f'{lista}', 'red'))
+            except KeyboardInterrupt:
+                print(colored('--- CLOSED ---', 'red'))
+                sys.exit(0)
+except (FileNotFoundError, KeyboardInterrupt):
+    print('\n')
+    sys.exit(0)
         
     
