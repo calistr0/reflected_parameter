91faf802aaad667c47816eaf7383c7af5fa7a740
calistr0 <88991900+calistr0@users.noreply.github.com>
2023-02-20 14:24:15 -0300

    Add files via upload

---
+++ rpv.py
@@ -0,0 +1,20 @@
+import requests, sys
+from termcolor import colored
+#programa para verificar se o parametro reflete na página
+
+arquivo = input(colored('Arquivo >>> ', 'green'))
+
+with open(arquivo, 'r') as arqv:
+    for lista in arqv.read().split('\n'):
+        try:
+            if '<content>' in requests.get(lista).text:
+                print(colored(f'{lista}', 'green'))
+                with open('rpv_response.txt', 'a') as rpv:
+                    rpv.write(f'\n{lista}')
+            else:
+                print(colored(f'{lista}', 'red'))
+        except KeyboardInterrupt:
+            print(colored('--- CLOSED ---', 'red'))
+            sys.exit(0)
+        
+    
